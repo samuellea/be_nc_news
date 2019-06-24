ebcd3808e0869ee1e31906601c4836f16df1fe85
samuellea <samuel.lea@live.co.uk>
2019-06-24 15:01:03 +0100

    First attempt implementing CORS

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const apiRouter = require('./routes/api-router');
 const { routeNotFound, handle500, handleCustomErrors, handlePsqlErrors, handleServerErrors, } = require('./errors');
+const cors = require('cors')
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors())
 
 app.use('/api', apiRouter);
 
